c3b56c510035b79ec27bfedfed919edc9c879626
usmanabbas2211 <usmanabbas2211@gmail.com>
2023-11-13 11:30:49 +0500

    added angular animation

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -29,10 +29,10 @@ const fadeInOutTransition = [
     animate('0.5s ease-in', style({ opacity: 1 })),
   ]),
   transition(':leave', [
-    style({
-      opacity: 1,
-    }),
-    animate('0.5s ease-out', style({ opacity: 0 })),
+    animate(
+      '0.5s ease-out',
+      style({ opacity: 0, transform: 'translateY(-20px)' })
+    ),
   ]),
 ];
 
--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,32 +1,11 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-} from '@angular/animations';
 import { ITask } from '../../../mock-taks';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const deleteTransition = [
-  state('in', style({ opacity: 1 })),
-
-  transition(':enter', [
-    style({ opacity: 0 }),
-    animate(600, style({ opacity: 1 })),
-  ]),
-  state('out', style({ opacity: 1 })),
-  transition(':leave', [
-    style({ opacity: 1 }),
-    animate(400, style({ opacity: 0 })),
-  ]),
-];
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.css'],
-  animations: [trigger('deleteEffect', deleteTransition)],
 })
 export class TaskItemComponent {
   @Input() task!: ITask;
